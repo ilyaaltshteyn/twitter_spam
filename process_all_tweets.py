6fc5b5f56fa74c1503c66f46eac9b7fa5cbf176b
ilya <ilyaaltshteyn@gmail.com>
2015-06-07 12:17:36 -0400

    added pyc to gitignore and made process_all_tweets able to run on its own, endlessly

--- process_all_tweets.py
+++ process_all_tweets.py
@@ -3,37 +3,44 @@
 # spam or not spam.
 import tweetPreprocessor
 from pymongo import MongoClient
+import time
 
 # Establish mongo info:
 client = MongoClient()
 db = client.tweets
 collect = db.random_sample_june7th
 
-# Pick up a bunch of tweets:
-found = collect.find({ 'spam_rating' : { '$exists' : False } } )
+# Pick up a bunch of tweets, but only if there are at least 5000 in there:
+while True:
+    if collect.count( { 'spam_rating' : { '$exists' : False } } ) >= 5000:
+        found = collect.find({ 'spam_rating' : { '$exists' : False } } ).limit(10000)
 
-# Turn them into two lists: one of ids, one of tweet text:
-tweet_ids = []
-tweet_texts = []
-while found.alive == True:
-    tweet = found.next()
-    tweet_texts.append(tweet['text'])
-    tweet_ids.append(tweet['id'])
+        # Turn them into two lists: one of ids, one of tweet text:
+        tweet_ids = []
+        tweet_texts = []
+        while found.alive == True:
+            tweet = found.next()
+            tweet_texts.append(tweet['text'])
+            tweet_ids.append(tweet['id'])
 
-db1 = tweetPreprocessor.tweetDatabase(tweet_texts)
-db1.identify_spam()
-db1.strip_and_lower_spam()
-spam_indices = db1.spam_indices
-spam_tweets = db1.spam_tweets_stripped_and_lowered
+        db1 = tweetPreprocessor.tweetDatabase(tweet_texts, batch_size = 10000)
+        db1.identify_spam()
+        db1.strip_and_lower_spam()
+        spam_indices = db1.spam_indices
+        spam_tweets = db1.spam_tweets_stripped_and_lowered
 
 # Use ids to insert spam_rating back into database:
 for tweet_index in range(len(tweet_ids)): # tweet_index is the id of the tweet in the python file!
     t_id = tweet_ids[tweet_index] # t_id is the id of the tweet in the mongodb!
+    if tweet_index % 100 == 0: print tweet_index
     if tweet_index in spam_indices:
-        collect.update({ 'id' : t_id}, {'spam_rating' : 1}, { 'upsert' : False})
+        collect.update({ 'id' : t_id}, {'spam_rating' : 1}, False)
     else:
-        collect.update({ 'id' : t_id}, {'spam_rating' : 0}, { 'upsert' : False})
+        collect.update({ 'id' : t_id}, {'spam_rating' : 0}, False)
 
+    # If there aren't at least 5000 tweets to work with, then sleep 60 secs:
+    else:
+        time.sleep(60)
 
 
 
