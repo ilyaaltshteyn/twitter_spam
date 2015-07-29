437ddb4ea93042a7355282269d6bc3053ba1a0d6
ilya altshteyn <ilyaaltshteyn@users.noreply.github.com>
2015-07-28 21:20:41 -0400

    Update stream_original_tweets.py

--- stream_original_tweets.py
+++ stream_original_tweets.py
@@ -14,12 +14,13 @@ collect = db.random_sample_june7th
 
 # Establish twitter info:
 delay = 8 # seconds
-
 api_details_path = '/Users/ilya/Projects/twitter_spam'
 api_details = []
+
 with open(api_details_path + '/api_details.txt', 'r') as a:
     info = a.readlines()
     api_details.append(info)
+    
 consumer_key, consumer_secret, access_token_key, access_token_secret = api_details[0][0].split(',')
 
 while True:
@@ -27,13 +28,13 @@ while True:
         api = TwitterAPI(consumer_key, consumer_secret,
                          access_token_key, access_token_secret)
         r = api.request('statuses/sample', {'language' : 'en'})
-        try:
-            for item in r.get_iterator():
+        for item in r.get_iterator():
+            try:
                 if 'retweeted_status' not in item:
                     collect.insert(item)
                 delay = max(8, delay/2)
-        except: 
-            continue
+            except:
+                continue
     except:
         print "Error"
         print time.ctime()
